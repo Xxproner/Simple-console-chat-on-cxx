70f463108f07cd1979cfb83889bf7cdf5b7c7612
Xxproner <pavelchessbur@mail.ru>
2024-03-07 13:53:10 +0300

    cmake

--- client.cpp
+++ client.cpp
@@ -6,18 +6,35 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <ctype.h>
 
+#include <string>
 #include <iostream>
+#include <fstream>
 #include <cassert>
 #include <thread>
 #include <algorithm>
 
 typedef unsigned long long ull;
 
+
 int Recv(int sockfd);
 
 int Send(int sockfd);
 
+void PossibleCommandsInfo();
+
+std::string BuildMessage(std::string& raw_msg, const std::string& name);
+
+std::string WhoRequest(const std::string& nickname, const std::string&);
+
+std::string Leave(const std::string& nickname, const std::string&);
+
+std::string Help(const std::string& nickname, const std::string&);
+
+std::string ChangeChannel(const std::string& nickname, const std::string& params);
+
+std::string SendPrivateMessage(const std::string& nickname, const std::string& params);
 
 int main(int argc, char* argv[])
 {
@@ -129,8 +146,6 @@ int main(int argc, char* argv[])
 	return EXIT_SUCCESS;
 }
 
-typedef unsigned long long ull;
-
 int Recv(int sockfd)
 {
 	fd_set master;
@@ -156,26 +171,42 @@ int Recv(int sockfd)
 			{
 				return EXIT_SUCCESS;
 			}
+
 			buf[bytes_recv] = '\0';
 			std::cout << buf << std::endl;
 		}
 
 	}
 
+	perror("Recv status");
 	return EXIT_SUCCESS;
 
 }
 
+
+
 int Send(int sockfd)
 {
+	bool keep_ = true;
 	std::cout << "What is your nickname? ";
 	std::cout.flush();
 	std::string msg;
 	std::getline(std::cin, msg);
 
-	ull bytes_send = 0;
-	while (true)
+	if (send(sockfd, msg.c_str(), msg.length(), 0) <= 0)
 	{
+		keep_ = false;
+	}
+
+	const std::string nickname = std::move(msg);
+	ull bytes_send = 0;
+	
+	while (keep_)
+	{
+		std::getline(std::cin, msg);
+		
+		// std::string builtMsg = BuildMessage(msg, nickname);
+		
 		bytes_send = send(sockfd, msg.c_str(), msg.length(), 0);
 
 		if (bytes_send < 0)
@@ -198,20 +229,41 @@ int Send(int sockfd)
 
 		}
 
+		if (msg == "/LEAVE" || msg == "/QUIT")
+			keep_ = false;
+
 		bytes_send = 0;
 
-		// if bytes_send < msg.length();
-
-		std::getline(std::cin, msg);
-
-		if (msg == "QUIT" || msg == "LEAVE")
-		{
-			// msg = "user:" + pseudo + "\ncommand:LEAVE";
-			shutdown(sockfd, SHUT_RD);
-			break;
-		}
 	}
 
+	perror("Send status");
+	shutdown(sockfd, SHUT_RD);
+
 	return EXIT_SUCCESS;
+}
+
+std::string BuildMessage(std::string& raw_msg, const std::string& name)
+{
+	std::string msg = raw_msg;
+	if (raw_msg[0] == '/') // command
+	{
+		while (isspace(raw_msg.back())) { raw_msg.pop_back(); }
+		size_t idx = raw_msg.find(' ');
+		std::string command, params; 
+
+		if (idx == std::string::npos)
+		{
+			command = raw_msg.substr(1, idx);
+			params = "";
+		}else 
+		{
+			command = raw_msg.substr(1, idx);
+			params = raw_msg.substr(idx + 1);
+		}
+
+		msg = "user:" + name + "\nCOMMAND:" + command + "\nOPTION:" + params;
+	}
+
+	return msg;
 }
 
